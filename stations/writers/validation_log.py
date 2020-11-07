e618243b4d6383a8c9d52aa77cf2d04a23cac72e
Johansson Johannes <a002028@ad.smhi.se>
2020-11-07 10:21:59 +0100

    ABC meta update

--- stations/writers/validation_log.py
+++ stations/writers/validation_log.py
@@ -6,12 +6,14 @@ Created on 2020-10-09 17:22
 @author: a002028
 
 """
+from abc import ABC
+
 import yaml
 # import json
 from stations.writers.writer import WriterBase
 
 
-class ValidationWriter(WriterBase):
+class ValidationWriter(WriterBase, ABC):
     """
     """
     def __init__(self, *args, **kwargs):
